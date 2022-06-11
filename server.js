8250d9a20ca7dbd74e7ad65d6dd583c65aabea43
TR742 <Tyrich1442@gmail.com>
2022-06-10 22:37:16 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const PORT = 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
