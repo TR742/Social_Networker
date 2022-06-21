d29629be9c0dc6bee15d8f81618a8297fdf06484
TR742 <Tyrich1442@gmail.com>
2022-06-20 22:37:06 -0700

    update server and connection

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const { connect, connection } = require('mongoose');
 
 
 const connectionString =
-    process.env.MONGODB_URI || 'mongodb://localhost:27017';
+    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialnetwork';
 
 connect(connectionString, {
     useNewUrlParser: true,
--- server.js
+++ server.js
@@ -9,4 +9,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port 3001!`);
+    });
+});
